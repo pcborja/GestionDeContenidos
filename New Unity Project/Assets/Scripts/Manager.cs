b468e9f165ecf077cc22f7166b5490c13221d712
Pablo Borja <pabloborjaaa@gmail.com>
2021-06-08 21:18:10 -0300

    Buscador funcionando rancio

--- New Unity Project/Assets/Scripts/Manager.cs
+++ New Unity Project/Assets/Scripts/Manager.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public List<Juego> juegos;
+    public InputField searchText;
 
-    void Update()
+    public void OnTextChanged(string text)
     {
         
     }
